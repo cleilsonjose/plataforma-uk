f9f4f0479c066cef3bff1576348d12c7b0f3d916
cleilsonjose <cleilsonrock@gmail.com>
2026-01-16 15:26:38 -0300

    feat: backend base gerado com perfis dev/prod e licença

---
+++ backend/src/test/java/com/plataforma/api/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataforma.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
